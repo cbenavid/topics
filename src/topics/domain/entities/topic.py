2a1a70e1941bf6911aed0fdf2785040517870a70
Carlos Benavides <carlos.benavides@capgemini.com>
2023-08-30 09:12:36 +0200

    feat: add priority to topic

--- src/topics/domain/entities/topic.py
+++ src/topics/domain/entities/topic.py
@@ -1,8 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from uuid import UUID
 
 
-@dataclass
+def _check_priority(priority: int) -> None:
+    if priority <= 0:
+        raise ValueError("Priority must be greater than zero")
+
+
+@dataclass(kw_only=True)
 class Topic:
     id: UUID
     content: str
+    priority: int = field(default=1)
+
+    def __post_init__(self) -> None:
+        _check_priority(self.priority)
--- tests/domain/entities/test_topic.py
+++ tests/domain/entities/test_topic.py
@@ -1,12 +1,36 @@
 from uuid import UUID
 
+import pytest
+
 from topics.domain.entities.topic import Topic
 
 
 class TestTopicEntity:
-    def test_can_create_topic(self) -> None:
+    def test_topic_can_be_created(self) -> None:
         topic = Topic(
-            id=UUID("8623788e-3c9c-421c-ab10-92dd10405ebe"), content="Some ref"
+            id=UUID("8623788e-3c9c-421c-ab10-92dd10405ebe"),
+            content="Some ref",
+            priority=1,
         )
         assert topic.id == UUID("8623788e-3c9c-421c-ab10-92dd10405ebe")
         assert topic.content == "Some ref"
+        assert topic.priority == 1
+
+    @pytest.mark.parametrize("priority", [-1, 0])
+    def test_topic_priority_must_be_greater_than_zero(self, priority: int) -> None:
+        with pytest.raises(ValueError, match="Priority must be greater than zero"):
+            build_topic(priority=priority)
+
+    def test_topic_has_default_priority_of_one(self) -> None:
+        topic = build_topic()
+        assert topic.priority == 1
+
+
+def build_topic(
+    id: UUID | None = None, content: str = "Some ref", priority: int | None = None
+) -> Topic:
+    id_ = id or UUID("8623788e-3c9c-421c-ab10-92dd10405ebe")
+    extra_values = {}
+    if priority is not None:
+        extra_values["priority"] = priority
+    return Topic(id=id_, content=content, **extra_values)
